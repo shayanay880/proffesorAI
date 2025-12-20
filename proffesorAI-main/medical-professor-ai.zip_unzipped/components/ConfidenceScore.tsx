0033b94c3a507926f89c043787675e99a2996aad
shayanay880 <167648493+shayanay880@users.noreply.github.com>
2025-12-20 09:14:19 +0330

    wdwdas

--- proffesorAI-main/medical-professor-ai.zip_unzipped/components/ConfidenceScore.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// This component is currently used inline or deprecated, keeping file to prevent build breaks.
-export const ConfidenceScore: React.FC = () => {
-  return null;
-};
